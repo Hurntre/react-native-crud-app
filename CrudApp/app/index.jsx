ec7176998abd5467875e60768df4f5efcdb8ccb2
Adefolaju <adefolajuariyo@gmail.com>
2025-08-14 09:52:14 +0100

    Add data storage and status bar style
    - Added @react-native-async-storage/async-storage for data persistence
    - introduced useEffect hooks to load get and set TodoApp data from storage

--- CrudApp/app/index.jsx
+++ CrudApp/app/index.jsx
@@ -1,28 +1,56 @@
-import {
-  Text,
-  View,
-  TextInput,
-  Pressable,
-  StyleSheet,
-  FlatList,
-} from "react-native";
+import { Text, View, TextInput, Pressable, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { ThemeContext } from "@/context/ThemeContext";
 import { data } from "@/data/todos";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
 import Octicons from "@expo/vector-icons/Octicons";
 import Animated, { LinearTransition } from "react-native-reanimated";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
-  const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
+  const [todos, setTodos] = useState([]);
   const [text, setText] = useState("");
   const { colorScheme, setColorScheme, theme } = useContext(ThemeContext);
 
   const [loaded, error] = useFonts({
     Inter_500Medium,
   });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("TodoApp");
+        const storageTodos = jsonValue != null ? JSON.parse(jsonValue) : null;
+
+        if (storageTodos && storageTodos.length > 0) {
+          setTodos(storageTodos.sort((a, b) => b.id - a.id));
+        } else {
+          // using the data that comes with the app if user has not started the app before
+          setTodos(data.sort((a, b) => b.id - a.id));
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, [])
+
+  useEffect(() => {
+    const storeData = async () => {
+      try {
+        const jsonValue = JSON.stringify(todos);
+        await AsyncStorage.setItem("TodoApp", jsonValue);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    storeData();
+  }, [todos]);
 
   // if the fonts are not loaded and there is no error, return null
   if (!loaded && !error) {
@@ -118,6 +146,7 @@ export default function Index() {
         itemLayoutAnimation={LinearTransition}
         keyboardDismissMode="on-drag"
       />
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
     </SafeAreaView>
   );
 }
